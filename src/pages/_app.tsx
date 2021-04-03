345118482817aab6b1584ec568d1b14b17849090
gustavo-sorati <gustavo.sorati@outlook.com>
2021-04-03 16:25:42 -0300

    nlw #4 ReactJS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import '../styles/global.css';
 
-import { ChallengesProvider } from '../contexts/ChallengeContext';
 import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
 //  Não precisamos colocar o <CountdownProvider> direto no App, pq não é um contexto que necessáriamente precisaria estar disponivel para todas as páginas, podemos adiciona-lo diretamente na página que queremos
 
   return (
-    <ChallengesProvider>
-        <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import { ChallengesProvider } from '../contexts/ChallengeContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
+
 import { ChanllengeBox } from '../components/ChanllengeBox';
 import { CompletedChanllenges } from '../components/CompletedChanllenges';
 import { Countdown } from '../components/Countdown';
 import {ExperienceBar} from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
-import { CountdownProvider } from '../contexts/CountdownContext';
 
 import styles from '../styles/pages/Home.module.css';
 
-export default function Home() {
+interface HomeProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+}
+
+
+const  Home: React.FC<HomeProps> = (props) => {
   return (
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}  
+      challengesCompleted={props.challengesCompleted}
+    >
+
       <div className={styles.container}>
         <Head>
           <title>Início | move.it</title>
@@ -30,5 +47,26 @@ export default function Home() {
           </section>
         </CountdownProvider>
       </div>
+    </ChallengesProvider>
+
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { level, currentExperience, challengesCompleted} = context.req.cookies;
+
+
+
+  // esse console.log é apenas para mostrar que esse trecho do código está sendo 
+  // executado no node.js   console.log(level);
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
+}
+
+export default Home; 
